785c729f65c3f6399881873af3c2f3fc216314f2
popularloner1t5 <olawoleisaac2@gmail.com>
2023-09-07 17:00:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 /**
- 	* main - this prints exactly the input on it
+ * main - this prints exactly the input on it
  * Return: always 1.
  */
 int main(void)
@@ -8,3 +8,4 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
