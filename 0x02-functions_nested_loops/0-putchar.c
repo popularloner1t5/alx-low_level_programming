9f63b1848263b0511c130bb9561a4b4d478a2fdd
popularloner1t5 <olawoleisaac2@gmail.com>
2023-09-12 16:04:05 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - function
- * Return: 0
+ * main - this is a function
+ * Return: returns 0
  */
 
 int main(void)
@@ -16,6 +16,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 
 }
