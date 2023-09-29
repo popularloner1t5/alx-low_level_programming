da928575cb5efaacc374aff42175649db26ee06d
popularloner1t5 <olawoleisaac2@gmail.com>
2023-09-28 18:46:38 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - recursive function to print to screen
+ *
+ * @s: pointer to string
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+}
