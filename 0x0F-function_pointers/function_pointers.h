3020a10ac6c131747df481aadabaf977ed2424fc
popularloner1t5 <olawoleisaac2@gmail.com>
2023-10-13 04:53:47 -0700

    0-print_name.c0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
 #ifndef _FUNCTIONS_POINTERS_H
 #define _FUNCTIONS_POINTERS_H
 
+
+#include <stddef.h>
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 
