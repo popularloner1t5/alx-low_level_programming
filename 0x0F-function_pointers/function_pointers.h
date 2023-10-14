3cdc5c0a326f96e72214a55b772020d3a251366f
popularloner1t5 <olawoleisaac2@gmail.com>
2023-10-13 17:58:12 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,12 @@
-#ifndef _FUNCTIONS_POINTERS_H
-#define _FUNCTIONS_POINTERS_H
+#ifndef _FUNCTIONS_POINTERS_H_
+#define _FUNCTIONS_POINTERS_H_
 
 
 #include <stddef.h>
 #include <stdlib.h>
 
-void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
