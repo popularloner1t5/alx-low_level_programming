0c485c3ccc8f9cf6f4805af6788e66419e25a050
popularloner1t5 <olawoleisaac2@gmail.com>
2023-11-02 16:29:35 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, 1024);
+		nwr = write(file_to, buf, nchars);
 			if (nwr == -1)
 			error_file(0, -1, argv);
 	}
@@ -59,10 +59,11 @@ int main(int argc, char *argv[])
 	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-				exit(100);
+			exit(100);
 
 	}
 	err_close = close(file_to);
+	if (err_close == -1)
 	{
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 	exit(100);
